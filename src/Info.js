cac13c6520400eafea774ec91626464f84ad94b2
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-13 14:16:36 -0500

    Added image

--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Headshot from "/images/javier-headshot.jpg";
 
 export default function Info() {
   return (
     <div className="info-section">
-      <img></img>
+      <img src={Headshot} alt="headshot"></img>
       <h1 className="name">Javier Guerra</h1>
       <h3 className="profession">Fullstack Developer</h3>
       <a className="portfolio">Portfolio</a>
