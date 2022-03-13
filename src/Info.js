ed87b2d169fe0cee563485a6aec715698c66b6e3
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-13 14:34:22 -0500

    Centered text and added border radius to top of image

--- src/Info.js
+++ src/Info.js
@@ -4,10 +4,12 @@ import Headshot from "./images/javier-headshot.jpg";
 export default function Info() {
   return (
     <div className="info-section">
-      <img src={Headshot} alt="headshot"></img>
+      <img className="my-headshot" src={Headshot} alt="headshot"></img>
       <h1 className="name">Javier Guerra</h1>
       <h3 className="profession">Fullstack Developer</h3>
-      <a className="portfolio">Portfolio</a>
+      <a>
+        <h4 className="portfolio">Portfolio</h4>
+      </a>
     </div>
   );
 }
