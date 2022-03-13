f83baa89bd8caee36924f9345cbf1412230f9ab0
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-13 15:24:32 -0500

    created email buttin and added space between text and icon

--- src/Info.js
+++ src/Info.js
@@ -7,9 +7,16 @@ export default function Info() {
       <img className="my-headshot" src={Headshot} alt="headshot"></img>
       <h1 className="name">Javier Guerra</h1>
       <h3 className="profession">Fullstack Developer</h3>
-      <a>
+      <span
+        href="https://javierguerra4.github.io/Javier-Guerra-Portfolio/"
+        target="_blank"
+      >
         <h4 className="portfolio">Portfolio</h4>
-      </a>
+      </span>
+      <button className="mail-button">
+        <i class="fa fa-envelope" aria-hidden="true"></i>Email
+      </button>
+      <button className="linkedin-button">LinkedIn</button>
     </div>
   );
 }
