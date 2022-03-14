4aa8cd77fa3274b4614b1b2168d908b8a99eeb1f
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-14 17:28:03 -0500

    Created About component with content

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about-interest-container">
+      <div className="about-container">
+        <h2 className="about">About</h2>
+        <p className="about-content">
+          I am a fullstack developer that enjoys learning about web-development
+          as a whole. I continue to increase the knowledge of the languages I
+          already know, and expose myself to new languages and frameworks.{" "}
+        </p>
+      </div>
+      <div className="interest-container">
+        <h2 className="interest">Interest</h2>
+        <p className="interest-content"></p>
+      </div>
+    </div>
+  );
+}
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Info from "./Info.js";
+import About from "./About.js";
 
 export default function Card() {
   return (
     <div className="card">
       <Info />
+      <About />
     </div>
   );
 }
