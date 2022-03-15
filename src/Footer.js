960911a01f00ef51f15174896d1556fc3b71b67a
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-15 00:28:35 -0500

    Styling done for all content

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <i class="fab fa-twitter-square fa-4x twitter-icon"></i>
+      <i class="fab fa-github-square fa-4x github-icon"></i>
+    </div>
+  );
 }
